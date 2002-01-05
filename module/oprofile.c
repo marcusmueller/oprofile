d6dd26b19c28dc54e639ef8e76a89a397c496f67
John Levon <moz@compsoc.man.ac.uk>
2002-01-05 03:17:48 +0000

    dump stop fixes

--- module/oprofile.c
+++ module/oprofile.c
@@ -1,4 +1,3 @@
-/* $Id: oprofile.c,v 1.27 2002/01/04 21:35:18 movement Exp $ */
 /* COPYRIGHT (C) 2000 THE VICTORIA UNIVERSITY OF MANCHESTER and John Levon
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
@@ -53,6 +52,8 @@ static u32 prof_on __cacheline_aligned;
 
 /* in the process of quitting ? */
 static int quitting;
+/* is partial_stop made ?  Re-using quitting for this purpose is obfuscated */
+static int partial_stop;
  
 static int op_major;
 
@@ -315,6 +316,12 @@ static void pmc_stop(void *info)
 
 inline static void pmc_select_start(uint cpu)
 {
+	/* we must make sure not to re-enable the counters
+	 * after a dump_stop
+	 */
+	if (partial_stop)
+		return;
+
 	if (cpu == op_cpu_id())
 		pmc_start(NULL);
 	else
@@ -323,6 +330,9 @@ inline static void pmc_select_start(uint cpu)
 
 inline static void pmc_select_stop(uint cpu)
 {
+	if (partial_stop)
+		return;
+
 	if (cpu == op_cpu_id())
 		pmc_stop(NULL);
 	else
@@ -556,6 +566,8 @@ static int oprof_release(struct inode *ino, struct file *file)
 
 	/* finished quitting */
 	quitting = 0;
+	/* the block on re-starting is over */
+	partial_stop = 0;
  
 	clear_bit(0, &oprof_opened);
 
@@ -760,10 +772,14 @@ out:
  */
 static void oprof_partial_stop(void)
 {
+	if (partial_stop)
+		return;
+
 	op_replace_syscalls();
 	smp_call_function(pmc_stop, NULL, 0, 1);
 	pmc_stop(NULL);
 	restore_nmi();
+	partial_stop = 1;
 }
  
 static int oprof_stop(void)
@@ -906,9 +922,7 @@ static void do_actual_dump(void)
 		}
 		spin_unlock(&note_lock);
 		oprof_ready[cpu] = 2;
-		if (!quitting) {
-			pmc_select_start(cpu);
-		}
+		pmc_select_start(cpu);
 	}
 	wake_up(&oprof_wait);
 }
