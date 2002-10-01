aa804ad2da5867271240a2c3c3db640992988a5a
John Levon <moz@compsoc.man.ac.uk>
2002-10-01 15:54:52 +0000

    Fail on P4 HT from Will

--- module/x86/cpu_type.c
+++ module/x86/cpu_type.c
@@ -17,6 +17,28 @@ MODULE_PARM(force_rtc, "i");
 MODULE_PARM_DESC(force_rtc, "force RTC mode.");
 static int force_rtc;
 
+/**
+ * p4_threads - determines the number of logical processor threads in a die
+ * 
+ * returns number of threads in p4 die (1 for non-HT processors)
+ */
+static int p4_threads(void)
+{
+	int processor_threads = 1;
+
+#ifdef CONFIG_SMP
+	if (test_bit(X86_FEATURE_HT, &current_cpu_data.x86_capability)) {
+		/* This it a Pentium 4 with HT, find number of threads */
+		int eax, ebx, ecx, edx;
+
+		cpuid (1, &eax, &ebx, &ecx, &edx);
+		processor_threads = (ebx >> 16) & 0xff;
+	}
+#endif /* CONFIG_SMP */
+
+	return processor_threads;
+}
+
 __init op_cpu get_cpu_type(void)
 {
 	__u8 vendor = current_cpu_data.x86_vendor;
@@ -47,10 +69,13 @@ __init op_cpu get_cpu_type(void)
 					return CPU_PII;
 				return CPU_PPRO;
 			case 0xf:
-				if (model <= 3)
-					/* a Pentium 4 processor */
-					return CPU_P4;
-				else
+				if (model <= 3) {
+					/* Cannot handle HT P4 hardware */
+					if (p4_threads()>1 )
+						return CPU_RTC;
+					else
+				  		return CPU_P4;
+				} else
 					/* Do not know what it is */
 					return CPU_RTC;
 			}
