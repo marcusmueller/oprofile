72c13fe0c331735ae59463ad43fc6d80eb371cce
John Levon <moz@compsoc.man.ac.uk>
2002-11-10 01:25:16 +0000

    More tweaks.  Confirmed to build on automake 1.7.1 / autoconf 2.54

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,15 @@
+#!/bin/sh
+
 # run to generate needed files not in CVS
 
 # this simple currently
 
-aclocal 
-autoheader
-automake --foreign --add-missing --copy
-autoconf
+run() {
+	echo "Running $1 ..."
+	$1
+}
+
+run aclocal
+run autoheader
+run "automake --foreign --add-missing --copy"
+run autoconf
