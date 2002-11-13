ca3bf24678640754d43571594a5b263485ccf666
Graydon Hoare <graydon@redhat.com>
2002-11-13 02:19:18 +0000

    reviewed and okayed by Philippe Elie (with comments added)
    --
    
    2002-11-12  Graydon Hoare  <graydon@redhat.com>
    
    	* autogen.sh: support overriding autotool binary names.

--- autogen.sh
+++ autogen.sh
@@ -2,14 +2,35 @@
 
 # run to generate needed files not in CVS
 
-# this simple currently
+# NB: if you run this file with AUTOMAKE, AUTOCONF, etc. environment
+# variables set, you *must* run "configure" with the same variables
+# set. this is because "configure" will embed the values of these variables
+# into the generated Makefiles, as @AUTOMAKE@, @AUTOCONF@ etc. and it will
+# trigger regeneration of configuration state using those programs when any
+# of Makefile.am etc. change.
 
 run() {
 	echo "Running $1 ..."
 	$1
 }
 
-run aclocal
-run autoheader
-run "automake --foreign --add-missing --copy"
-run autoconf
+set -e
+
+ACLOCAL=${ACLOCAL:-aclocal}
+AUTOHEADER=${AUTOHEADER:-autoheader}
+AUTOMAKE=${AUTOMAKE:-automake}
+AUTOCONF=${AUTOCONF:-autoconf}
+
+if $AUTOMAKE --version | grep -q 1.4
+	then
+	echo ""
+	echo "Automake 1.4 not supported. please set \$AUTOMAKE"
+	echo "to point to a newer automake, or upgrade."
+	echo ""
+	exit 1
+fi
+
+run $ACLOCAL
+run $AUTOHEADER
+run "$AUTOMAKE --foreign --add-missing --copy"
+run $AUTOCONF
