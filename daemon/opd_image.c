e03ef896d0ca42a64ab8225c63d3dfbb170b6965
Philippe Elie <phil.el@wanadoo.fr>
2003-09-05 15:54:35 +0000

    After a context switch ensure we update correctly the current image.

--- daemon/opd_image.c
+++ daemon/opd_image.c
@@ -513,6 +513,39 @@ static void get_tgid(struct transient * trans)
 }
 
 
+/**
+ * There is two case where we will not get a cookie switch after a context
+ * switch so we must take care to update properly trans->image.
+ * These two case are:
+ * - context switch followed by a kernel sample
+ * - context switch followed by a sample to the same shared libs as
+ * the last sample went.
+ */
+static void ctx_switch_set_image(struct transient * trans)
+{
+	if (trans->app_cookie) {
+		if (trans->cookie)
+			// if the next sample goes to the same shared libs
+			// than the previous we will not get a cookie_switch
+			// so we force trans->image now. Note it means we
+			// can create invalid image where invalid means
+			// process A don't use shared libs B but we can create
+			// an image for A/B, indeed we will never get samples
+			// to this image so no big deal.
+			trans->image = opd_get_image(trans->cookie,
+			                             trans->app_cookie);
+		else
+			// If cookie == 0 and a kernel sample follows this
+			// we need to make sure that it is attributed to the
+			// right application, namely the one we just swiched to
+			trans->image = opd_get_image(trans->app_cookie,
+			                             trans->app_cookie);
+	} else {
+		trans->image = 0;
+	}
+}
+
+
 static void code_ctx_switch(struct transient * trans)
 {
 	if (!enough_remaining(trans, 2)) {
@@ -523,14 +556,7 @@ static void code_ctx_switch(struct transient * trans)
 	trans->pid = pop_buffer_value(trans);
 	trans->app_cookie = pop_buffer_value(trans);
 
-	/* This is a corner case - if a kernel sample follows this,
-	 * we need to make sure that it is attributed to the
-	 * right application, namely the one we just switched into.
-	 */
-	if (trans->app_cookie)
-		trans->image = opd_get_image(trans->app_cookie, trans->app_cookie);
-	else
-		trans->image = 0;
+	ctx_switch_set_image(trans);
 
 	/* Look for a possible tgid postscript */
 	get_tgid(trans);
